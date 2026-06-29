f69ca64ba74c1063d962071b619523651c6fcdbb
mrsilverfog-hash <mrsilverfog@gmail.com>
2026-06-29 10:26:45 +0200

    Add files via upload

---
+++ src/main/java/net/tropimon/calculatortropi/CalculatortropiClient.java
@@ -0,0 +1,15 @@
+package net.tropimon.calculatortropi;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CalculatortropiClient implements ClientModInitializer {
+    public static final String MOD_ID = "calculatortropi";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("[Calculatortropi] Mod chargé avec succès !");
+    }
+}
