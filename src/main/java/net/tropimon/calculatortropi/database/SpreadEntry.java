6c8acff5b92f36226744c021c2899e6303b91a89
mrsilverfog-hash <mrsilverfog@gmail.com>
2026-06-29 15:47:01 +0200

    Update SpreadEntry.java

--- src/main/java/net/tropimon/calculatortropi/database/SpreadEntry.java
+++ src/main/java/net/tropimon/calculatortropi/database/SpreadEntry.java
@@ -2,14 +2,23 @@ package net.tropimon.calculatortropi.database;
 
 import net.tropimon.calculatortropi.calc.Nature;
 
+import java.util.Collections;
+import java.util.List;
+
 public class SpreadEntry {
     public final String objet;
     public final Nature nature;
     public final int evPv, evAtk, evDef, evSpa, evSpd, evSpe;
     public final String talent;
+    public final List<String> topMoves;
 
     public SpreadEntry(String objet, Nature nature, int evPv, int evAtk, int evDef,
                         int evSpa, int evSpd, int evSpe, String talent) {
+        this(objet, nature, evPv, evAtk, evDef, evSpa, evSpd, evSpe, talent, Collections.emptyList());
+    }
+
+    public SpreadEntry(String objet, Nature nature, int evPv, int evAtk, int evDef,
+                        int evSpa, int evSpd, int evSpe, String talent, List<String> topMoves) {
         this.objet = objet;
         this.nature = nature;
         this.evPv = evPv;
@@ -19,5 +28,6 @@ public class SpreadEntry {
         this.evSpd = evSpd;
         this.evSpe = evSpe;
         this.talent = talent;
+        this.topMoves = topMoves;
     }
 }
