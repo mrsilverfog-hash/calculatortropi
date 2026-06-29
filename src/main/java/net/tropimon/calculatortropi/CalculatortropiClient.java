409d3950b62cec8b61cdaf038ffc9240122ba888
mrsilverfog-hash <mrsilverfog@gmail.com>
2026-06-29 11:07:30 +0200

    Update CalculatortropiClient.java

--- src/main/java/net/tropimon/calculatortropi/CalculatortropiClient.java
+++ src/main/java/net/tropimon/calculatortropi/CalculatortropiClient.java
@@ -2,6 +2,7 @@ package net.tropimon.calculatortropi;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.tropimon.calculatortropi.command.CalcTeamCommand;
 import net.tropimon.calculatortropi.command.CalcTestCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,10 @@ public class CalculatortropiClient implements ClientModInitializer {
     public void onInitializeClient() {
         LOGGER.info("[Calculatortropi] Mod chargé avec succès !");
         ClientCommandRegistrationCallback.EVENT.register(
-                (dispatcher, registryAccess) -> CalcTestCommand.register(dispatcher)
+                (dispatcher, registryAccess) -> {
+                    CalcTestCommand.register(dispatcher);
+                    CalcTeamCommand.register(dispatcher);
+                }
         );
     }
 }
